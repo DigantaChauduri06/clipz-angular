20f6314d9714df4bebbe6ba6f04cd938c3c7d22f
Diganta Chaudhuri <digantachaudhuri03@gmail.com>
2023-03-14 10:59:28 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// modules
+import { UsersModule } from './users/users.module';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  // declarations take cares of Components, Directives, Pipes
+  declarations: [AppComponent, NavbarComponent],
+  // Import take cares of Modules
+  imports: [BrowserModule, AppRoutingModule, UsersModule],
+  // providers take cares of Services
   providers: [],
-  bootstrap: [AppComponent]
+  // bootstrap is the root component of the app
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
